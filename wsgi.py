55982ab01f6dc519d415932cfe165fe62915ef7b
Neliall2 <nicita.silenkov@yandex.ru>
2025-05-08 01:19:04 +0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,13 @@ import os
 import asyncio
 from threading import Thread
 
-def run_bot():
-    asyncio.run(main())
+def run_flask():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
-    # Запускаем бота в отдельном потоке
-    Thread(target=run_bot).start()
+    # Запускаем Flask в отдельном потоке
+    Thread(target=run_flask).start()
     
-    # Запускаем Flask
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port) 
+    # Запускаем бота в основном потоке
+    asyncio.run(main()) 
